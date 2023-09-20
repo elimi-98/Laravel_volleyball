c1cd459c076df1e11b5bf1f3a295a14922397974
eli-mi98 <elisiavmi@gmail.com>
2023-09-20 18:28:17 +0200

    creación de relaciones entre user->equipo->partido

--- app/Models/Partido.php
+++ app/Models/Partido.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Equipo; 
 
 class Partido extends Model
 {
     use HasFactory;
+    protected $guarded = [];
+    // Relación inversa con Equipo
+    public function local(){
+        return $this->belongsTo(Equipo::class, 'equipo_local');
+    }
+    //Relación inversa con Partido
+    public function visitante() {
+        return $this->belongsTo(Equipo::class,'equipo_visitante');
+    }
 }
